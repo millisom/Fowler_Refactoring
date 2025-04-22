21a923c4327be7b2a928f21d7840a4521d203874
Emili Sommer <eliza.liebt.sommer@gmail.com>
2025-04-22 11:26:12 +0200

    Move getCharge() logic from Customer to Rental

--- .idea/src/Rental.java
+++ .idea/src/Rental.java
@@ -1,14 +1,37 @@
 class Rental {
     private Movie movie;
     private int daysRented;
+
     public Rental(Movie newmovie, int newdaysRented) {
         movie = newmovie;
         daysRented = newdaysRented;
     }
+
     public int getDaysRented() {
         return daysRented;
     }
+
     public Movie getMovie() {
         return movie;
+    }
+
+    public double getCharge() {
+        double thisAmount = 0;
+        switch (movie.getPriceCode()) {
+            case Movie.REGULAR:
+                thisAmount += 2;
+                if (daysRented > 2)
+                    thisAmount += (daysRented - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                thisAmount += daysRented * 3;
+                break;
+            case Movie.CHILDRENS:
+                thisAmount += 1.5;
+                if (daysRented > 3)
+                    thisAmount += (daysRented - 3) * 1.5;
+                break;
+        }
+        return thisAmount;
     }
 }
